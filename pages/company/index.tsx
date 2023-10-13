6af54f3efd75b0bf191e23f43bd8644d834cb8c1
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-10-13 23:37:21 +0900

    company

--- pages/company/index.tsx
+++ pages/company/index.tsx
@@ -1,20 +1,29 @@
 import React from "react";
 import styles from "./index.module.css";
 import Image from "next/image";
+import { useState } from "react";
 
-function index() {
+function Index() {
+  const [companyName, setCompanyName] = useState("株式会社フルーリオ");
+  const [companyDetail, setCompanyDetail] = useState(
+    `株式会社フルーリオは
+    TypeScriptのみでフルスタックアプリを作成可能な
+    フレームワーク｢Frourio｣を提供しています`
+  );
+  const [companySlogan, setCompanySlogan] = useState("会社のスローガン");
+
   return (
     <div className={styles.container}>
-      <div className={styles.topBox}>企業名</div>
+      <div className={styles.topBox}>{companyName}</div>
       <div className={styles.middleBox}>
         <div className={styles.leftBox}>
-          <div className={styles.text}>スローガン</div>
-          <div className={styles.text}>会社詳細</div>
+          <div className={styles.text}>{companySlogan}</div>
+          <div className={styles.text}>{companyDetail}</div>
         </div>
         <div className={styles.rightBox}>
           <Image
             src="/images/ignoreImage.jpg"
-            width={300}
+            width={480}
             height={300}
             alt="logo"
           />
@@ -22,10 +31,10 @@ function index() {
       </div>
 
       <div className={styles.bottomBox}>
-        <div className={styles.text}>募集要項</div>
+        <div className={styles.text}>ここに募集要項</div>
       </div>
     </div>
   );
 }
 
-export default index;
+export default Index;
