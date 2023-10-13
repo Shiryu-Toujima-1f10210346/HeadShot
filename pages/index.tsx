66e98f80c99755ac061ba1212b31f6c69a2b24f7
Shiryu-Toujima-1f10210346 <s1f102103460@iniad.org>
2023-10-14 05:45:26 +0900

    会社詳細　偏差値　オファーリスト

--- pages/index.tsx
+++ pages/index.tsx
@@ -1,5 +1,8 @@
 import Chart from "@/components/Chart";
+import Deviation from "@/components/Deviation";
+import OfferList from "@/components/offerList";
 import React, { useState } from "react";
+import { Box, Paper, Typography } from "@mui/material";
 
 function Index() {
   const [data, setData] = useState([
@@ -10,6 +13,19 @@ function Index() {
     { ability: "わからん", A: 85, B: 90, fullMark: 150 },
     { ability: "ほげ", A: 65, B: 85, fullMark: 150 },
   ]);
+
+  const newsData = [
+    { companyName: "株式会社フルーリオ", description: "TypeScriptCh" },
+    {
+      companyName: "株式会社ほげんぽつ",
+      description: "めちゃくちゃブラック企業です！非推奨",
+    },
+  ];
+  const [limit, setLimit] = useState(7); // 新しいstate変数
+
+  const handleShowMore = () => {
+    setLimit((prevLimit) => prevLimit + 7);
+  };
 
   const handleInputChange = (event: React.ChangeEvent<HTMLInputElement>) => {
     const newData = [...data];
@@ -22,6 +38,33 @@ function Index() {
     <div>
       <input type="number" onChange={handleInputChange} placeholder="AIM" />
       <Chart data={data} width={500} height={500} outerRadius={200} />
+      <Deviation />
+      <br />
+      <Paper elevation={3} style={{ display: "inline-block", padding: "20px" }}>
+        {newsData.slice(0, limit).map(
+          (
+            news,
+            index // .sliceを追加
+          ) => (
+            <OfferList
+              key={index}
+              companyName={news.companyName}
+              description={news.description}
+              width="30vw"
+              height="80%"
+            />
+          )
+        )}
+        <Typography
+          style={{ textAlign: "right" }}
+          onClick={handleShowMore}
+          padding="5px"
+          color="primary.main"
+          fontWeight="bold"
+        >
+          もっと見る
+        </Typography>
+      </Paper>
     </div>
   );
 }
